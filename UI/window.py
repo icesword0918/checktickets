de714bd89021ffabc40ed71c824d07e594ac40ff
git <152119156@qq.com>
2019-12-20 11:11:58 +0800

    完成背景设置

--- UI/window.py
+++ UI/window.py
@@ -6,32 +6,43 @@
 #
 # WARNING! All changes made in this file will be lost!
 
-
+import sys
 from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtGui import QPalette,QPixmap,QColor    #导入QtGui模块
 
 
-class Ui_mainWindow(object):
-    def setupUi(self, mainWindow):
-        mainWindow.setObjectName("mainWindow")    # 设置窗体对象名称
-        mainWindow.resize(960, 786)               # 设置窗体大小
-        mainWindow.setMinimumSize(QtCore.QSize(960, 786))  # 主窗体最小值
-        mainWindow.setMaximumSize(QtCore.QSize(960, 786))  # 主窗体最大值
-        self.centralwidget = QtWidgets.QWidget(mainWindow)   # 主窗体的widget控件
-        self.centralwidget.setObjectName("centralwidget")    # 设置对象名称
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")  # 设置窗体对象名称
+        MainWindow.resize(960, 786)    # 设置窗体大小
+        MainWindow.setMinimumSize(QtCore.QSize(960, 786))  # 主窗体最小值
+        MainWindow.setMaximumSize(QtCore.QSize(960, 786))  # 主窗体最大值
+        self.centralwidget = QtWidgets.QWidget(MainWindow)   # 主窗体的Widget控件
+        self.centralwidget.setObjectName("centralwidget")  # 设置对象名称
+        # 通过label控件显示顶部图片
         self.label_title_img = QtWidgets.QLabel(self.centralwidget)
         self.label_title_img.setGeometry(QtCore.QRect(0, 0, 960, 91))
         self.label_title_img.setText("")
         self.label_title_img.setObjectName("label_title_img")
+        title_img=QPixmap('..\\img\\bg.png')    # 打开顶部位图
+        self.label_title_img.setPixmap(title_img)    # 设置调色板
         self.labe_train_img = QtWidgets.QLabel(self.centralwidget)
         self.labe_train_img.setGeometry(QtCore.QRect(0, 90, 960, 161))
         self.labe_train_img.setText("")
         self.labe_train_img.setObjectName("labe_train_img")
         self.tableView = QtWidgets.QTableView(self.centralwidget)
-        self.tableView.setGeometry(QtCore.QRect(0, 250, 960, 461))
+        self.tableView.setGeometry(QtCore.QRect(0, 250, 960, 450))
         self.tableView.setObjectName("tableView")
+        # 查询部分的widget
         self.widget_query = QtWidgets.QWidget(self.centralwidget)
         self.widget_query.setGeometry(QtCore.QRect(0, 90, 960, 80))
         self.widget_query.setObjectName("widget_query")
+        # 开启自动填充背景
+        self.widget_query.setAutoFillBackground(True)
+        palette=QPalette()    # 调色板类
+        # 设置背景图片
+        palette.setBrush(QPalette.Background,QtGui.QBrush(QtGui.QPixmap('..\\img\\bg1.png')))
+        self.widget_query.setPalette(palette)   # 为控件设置对应的调色板即可
         self.label = QtWidgets.QLabel(self.widget_query)
         self.label.setGeometry(QtCore.QRect(30, 30, 71, 20))
         self.label.setObjectName("label")
@@ -74,28 +85,40 @@ class Ui_mainWindow(object):
         self.checkBox_K = QtWidgets.QCheckBox(self.widget_checkBox)
         self.checkBox_K.setGeometry(QtCore.QRect(650, 30, 71, 16))
         self.checkBox_K.setObjectName("checkBox_K")
-        mainWindow.setCentralWidget(self.centralwidget)
-        self.menubar = QtWidgets.QMenuBar(mainWindow)
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
         self.menubar.setGeometry(QtCore.QRect(0, 0, 960, 23))
         self.menubar.setObjectName("menubar")
-        mainWindow.setMenuBar(self.menubar)
-        self.statusbar = QtWidgets.QStatusBar(mainWindow)
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
         self.statusbar.setObjectName("statusbar")
-        mainWindow.setStatusBar(self.statusbar)
+        MainWindow.setStatusBar(self.statusbar)
 
-        self.retranslateUi(mainWindow)
-        QtCore.QMetaObject.connectSlotsByName(mainWindow)
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
-    def retranslateUi(self, mainWindow):
+    def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
-        mainWindow.setWindowTitle(_translate("mainWindow", "车票查询"))
-        self.label.setText(_translate("mainWindow", "出发地"))
-        self.label_2.setText(_translate("mainWindow", "目的地"))
-        self.label_3.setText(_translate("mainWindow", "出发日"))
-        self.pushButton.setText(_translate("mainWindow", "查询"))
-        self.label_4.setText(_translate("mainWindow", "车次类型"))
-        self.checkBox_G.setText(_translate("mainWindow", "GC-高铁"))
-        self.checkBox_D.setText(_translate("mainWindow", "D-动车"))
-        self.checkBox_Z.setText(_translate("mainWindow", "Z-直达"))
-        self.checkBox_T.setText(_translate("mainWindow", "T-特快"))
-        self.checkBox_K.setText(_translate("mainWindow", "K-快速"))
+        MainWindow.setWindowTitle(_translate("MainWindow", "车票查询"))
+        self.label.setText(_translate("MainWindow", "出发地"))
+        self.label_2.setText(_translate("MainWindow", "目的地"))
+        self.label_3.setText(_translate("MainWindow", "出发日"))
+        self.pushButton.setText(_translate("MainWindow", "查询"))
+        self.label_4.setText(_translate("MainWindow", "车次类型"))
+        self.checkBox_G.setText(_translate("MainWindow", "GC-高铁"))
+        self.checkBox_D.setText(_translate("MainWindow", "D-动车"))
+        self.checkBox_Z.setText(_translate("MainWindow", "Z-直达"))
+        self.checkBox_T.setText(_translate("MainWindow", "T-特快"))
+        self.checkBox_K.setText(_translate("MainWindow", "K-快速"))
+
+
+def show_MainWindow():
+    app=QtWidgets.QApplication(sys.argv)
+    MainWindow=QtWidgets.QMainWindow()
+    ui=Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
+
+if __name__=="__main__":
+    show_MainWindow()
